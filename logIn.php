47d184b3a9bc7f313fdac9ef30611e9f67f0b635
João Afonso Pereira <joaoappereira@gmail.com>
2018-11-22 01:00:07 +0000

    log

---
+++ logIn.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Reddit</title>
+    <link href="css/forms.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="login">
+        <form class="login_content" action="action_login.php" method="post">
+            <div class="title_container">
+                <h1 class="login-form-title">Login</h1>
+            </div>
+
+            <div class="input_container">
+                <input type="text" placeholder="Enter username" name="username" required>
+            </div>
+
+            <div class="input_container">
+                <input type="password" placeholder="Enter password" name="password" required>
+            </div>
+
+            <div class="input_container">
+                <button type="submit">Login</button>
+            </div>
+            
+            <?php if (isset($_GET['error'])) {
+                echo "<p>" . $error = $_GET['error'] . "</p>";
+            } ?>
+        </form>
+    </div>
+
+</body>
+
+</html>
